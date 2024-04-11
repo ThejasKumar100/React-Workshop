666565070d3ac3ee940a1fe16e84d2b82d9b9531
Ashar Alvany <ashzee10000@gmail.com>
2024-04-11 15:49:55 -0500

    challenge progress

--- src/components/ClubCard.tsx
+++ src/components/ClubCard.tsx
@@ -14,6 +14,7 @@ interface ClubData {
   approved: string;
   profileImage: string | null;
   soc: boolean;
+  url: string;
 }
 
 const ClubCard = (props: ClubCardProps) => {
@@ -48,7 +49,7 @@ const ClubCard = (props: ClubCardProps) => {
   const name = club.name.length > 20 ? club.name.slice(0, 30) + '...' : club.name;
 
   return (
-    <div className="justify-self-center transition-all flex h-full min-h-[400px] min-w-[300px] max-w-xs flex-col self-center justify-between rounded-lg bg-white shadow-lg md:min-h-[600px] cardCont">
+    <div className="justify-self-center transition-all flex h-full min-h-[400px] min-w-[300px] max-w-xs flex-col self-center justify-between rounded-lg bg-white shadow-lg md:min-h-[600px] hover:scale-105">
       <div className="relative h-48 overflow-hidden rounded-t-lg sm:h-56 md:h-64 lg:h-72">
         <img
           src={club.profileImage ? club.profileImage : club.image}
@@ -67,15 +68,18 @@ const ClubCard = (props: ClubCardProps) => {
         <p className="text-base text-slate-600 md:text-sm">{desc}</p>
       </div>
       <div className="m-5 mt-auto flex flex-row space-x-2">
-        <button className="bg-cornflower-500 text-xs font-extrabold text-white transition-colors hover:bg-royal mr-2 rounded-2xl px-4 py-2">
+        <button onClick={()=>{window.open(club.url, "_blank")}} className="bg-cornflower-500 text-xs font-extrabold text-white transition-colors hover:bg-royal mr-2 rounded-2xl px-4 py-2">
           Join
         </button>
-        <Link
+        <button onClick={()=>{window.open(`https://jupiter.utdnebula.com/directory/${club.id}`, "_blank")}} className="rounded-2xl bg-cornflower-50 px-4 py-2 text-xs font-extrabold text-blue-primary transition-colors hover:bg-cornflower-100">
+        Learn More
+        </button>
+        {/* <Link
           href={`https://jupiter.utdnebula.com/directory/${club.id}`}
           className="rounded-2xl bg-cornflower-50 px-4 py-2 text-xs font-extrabold text-blue-primary transition-colors hover:bg-cornflower-100"
         >
-          Learn More
-        </Link>
+        Learn More
+        </Link> */}
       </div>
     </div>
   );
